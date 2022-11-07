ff6d00841bf60565b5e54524f70614d394d54b29
ondayary@gmail.com <ondayary@gmail.com>
2022-11-07 14:13:56 +0900

    PathVariable로부터 넘어온 값 넘기기

--- src/main/java/com/mustache/bbs/controller/MustacheController.java
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -10,14 +10,14 @@ public class MustacheController {
 
     @GetMapping(value = "/hi") // mustacheCon 메서드가 /hi 라는 요청으로 동작한다.
     public String mustacheCon(Model model) {
-        model.addAttribute("username", "rok"); // view에 값을 넘기겠다.
+        model.addAttribute("username", "daon"); // view에 값을 넘기겠다.
         return "greetings"; // mustacheCon 메서드는 반환값으로 greetings 페이지를 보여준다.
     }
 
     // PathVariable로부터 넘어온 값 넘기기
     @GetMapping(value = "/hi/{id}")
     public String mustacheCon2(@PathVariable String id, Model model) {
-        model.addAttribute("username", "rok");
+        model.addAttribute("username", "daon");
         model.addAttribute("id", id);// view에 값을 넘기겠다.
         return "greetings"; // greetings라는 이름의 view를 리턴
     }
