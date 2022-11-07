c9d6cc483dcbeed81caf176378f3fd9e5cdbf1e9
ondayary@gmail.com <ondayary@gmail.com>
2022-11-07 14:19:24 +0900

    domain.dto.ArticleDto.java

---
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -0,0 +1,16 @@
+package com.mustache.bbs.domain.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ArticleDto {
+    private Long id;
+    private String title;
+    private String content;
+
+    public ArticleDto(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+}
