4fd08df9b825a7fc39c95cb3ab03edeb870ef1a7
ondayary@gmail.com <ondayary@gmail.com>
2022-11-07 15:33:51 +0900

    DAO 의 추상화된 버전 Repository 만들기

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -1,6 +1,8 @@
 package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.dto.ArticleDto;
+import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j // 로깅을 위한 어노테이션 log를 사용할 수있다.
 public class ArticleController {
 
+    // DAO의 추상화된 버전 Repository 만들기
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping(value = "/new")
     public String newArticleForm() {
         return "articles/new";
@@ -20,8 +29,11 @@ public class ArticleController {
 
     @PostMapping(value = "/posts")
     public String createArticleForm(ArticleDto form) {
+
         // 실무에서 로그를 쓴다(서버에서 일어나는 일을 기록하는것)
         log.info(form.toString());
+
+        Article article = form.toEntity();
         return "";
     }
 }
