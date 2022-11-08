96c29fce44baa56921baa8952d4467e517d9fe2d
ondayary@gmail.com <ondayary@gmail.com>
2022-11-08 17:17:17 +0900

    조회 기능 만들기 1. Controller에 /articles/{id} 연결 2. id를 받아서 select해서 Article로 받아오는 기능 —> Model 3. Article로 받아온 데이터를 출력하는 화면
    list화면 만들기
    1. /list일때 /일때 모두 list로 가도록
    2. findAll()한 결과를 list로 넘김

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -5,9 +5,14 @@ import com.mustache.bbs.domain.entity.Article;
 import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -21,10 +26,31 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
+    @GetMapping(value = "/list")
+    public String list(Model model) {
+        // 정보를 담을 list 선언
+        List<Article> articles = articleRepository.findAll(); // 리포지토리에 있는 전부를 찾아온다.
+        model.addAttribute("articles", articles);
+        return "articles/list";
+    }
+
     @GetMapping(value = "/new")
     public String newArticleForm() {
         return "articles/new";
         // /articles/new url로 들어오면 artices/new.mustache를 출력하는 메소드
+    }
+
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if (!optArticle.isEmpty()) {
+            // Optional.get() ---> Article
+            model.addAttribute("article", optArticle.get());
+            return "articles/show";
+        } else {
+            return "articles/error";
+        }
     }
 
     @PostMapping(value = "/posts")
@@ -34,6 +60,7 @@ public class ArticleController {
         log.info(form.toString());
 
         Article article = form.toEntity();
-        return "";
+        articleRepository.save(article);
+        return String.format("redirect:/articles/%d", article.getId());
     }
 }
